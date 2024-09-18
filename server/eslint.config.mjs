3f5f11c05b247b990f91797b5bff06930b080927
Mayolito Waje <mayolito.waje08@gmail.com>
2024-09-18 19:52:04 +0800

    init

---
+++ server/eslint.config.mjs
@@ -0,0 +1,38 @@
+/* eslint no-underscore-dangle: 0 */
+/* eslint @typescript-eslint/naming-convention: 0 */
+
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { fixupConfigRules } from '@eslint/compat';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...fixupConfigRules(compat.extends('airbnb-base')),
+  ...compat.extends('airbnb-typescript/base'),
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsConfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      'import/no-extraneous-dependencies': false,
+    }
+  },
+];
